919075fe2a9c7f413092a55add50560860715662
RUTHERLING <rutherling@gmail.com>
2016-05-15 13:02:20 -0700

    added console.log messages

--- ruthAssignment/lab5/compute.js
+++ ruthAssignment/lab5/compute.js
@@ -1,12 +1,14 @@
 //Problem 1: sum
 function sum(a, b) {
   var s = a + b;
+  console.log('The sum of ' + a + ' and ' + b + ' is ' + s);
   return s;
 };
 
 //Problem 2: multiplying
 function multiply(a, b) {
   var m = a * b;
+  console.log('The product of  ' + a + ' and ' + b + ' is ' + m + '.');
   return m;
 };
 
@@ -14,8 +16,11 @@ function multiply(a, b) {
 function sumAndMultiply(a, b, c) {
   var s = sum(sum(a,b),c);
   var m = multiply(multiply(a,b),c);
-
-  return [s, m];
+  var sm = [s,m];
+  console.log('Problem 3 output: ' + [s,m]);
+  console.log(a + ' and ' + b + ' and ' + ' and ' + c + ' sum to ' + s + '.');
+  console.log('The numbers ' + a + ' and ' + b + ' and ' + c + ' have a product of ' + m + '.');
+  return sm;
 };
 
 //Problem 4: sumArray
@@ -26,6 +31,21 @@ function sumArray(a) { //a must be an array. maybe we can test for what happens 
 
   }
   var finalSum = partialSum;
+  console.table(a); //shows the array passed into the function
+
+  //Console log the message to display each of the items in the array.
+  var storeString = '';
+  for (var i = 0; i < a.length - 1; i++) {
+    var currentNumber = a[i];
+    if(i < (a.length - 1)){
+      //concatenate the next item in the array to string and add a comma.
+      currentNumber = a[i];
+      storeString = storeString.concat(currentNumber + ', ');
+    }
+    //put on the last number without a comma
+    var message = storeString + a[a.length - 1];
+  }
+  console.log(message + ' was passed in as an array of numbers, and ' + finalSum + ' is their sum.');
   return finalSum;
 };
 
@@ -35,5 +55,19 @@ function multiplyArray(a) {
   for (var i = 0; i < a.length; i++) {
     partialProduct = multiply(partialProduct,a[i]);
   }
+  console.table(a); //shows the array passed into the function
+
+  //console log the message to display each of the itmes in the aray.
+  var storeString = '';
+  for (var i = 0; i < a.length - 1; i++){
+    var currentNumber = a[i];
+    if(i < a.length - 1){
+      //concatenate the next item in the array to the string and add a comma
+      currentNumber = a[i];
+      storeString = storeString.concat(currentNumber + ', ');
+    }
+    var message = storeString + a[a.length - 1];
+  }
+  console.log(message + ' was passed in as an array of numbers, and ' + partialProduct + ' is their product.');
   return partialProduct;
 };

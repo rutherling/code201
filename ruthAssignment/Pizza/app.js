600e69edec7c01c534c855888d74927fa0f29ad2
RUTHERLING <rutherling@gmail.com>
2016-05-16 18:34:58 -0700

    loops through neighborhoods and hours to create objects and array

--- ruthAssignment/Pizza/app.js
+++ ruthAssignment/Pizza/app.js
@@ -38,16 +38,60 @@ function getValues(minPizza, maxPizza, minDelivery){ //intentionally did not inc
   };
 }
 
-//Loop through each neighborhood
-//Loop through each hour
-//8am to 10am
+//Function to push countPizza, countDelivery, and countDriver every hour, for each neighborhood
+function storeValues(){
+  //create empty array to store all the neighborhoods.
+  var finalArray = [];
 
-//11am to 1pm
+  //outer loop iterates through neighborhoods
+  for(i = 0; i < neighborhood.length; i++){
+    //create an object for each neighborhood pizzeria
+    var pizzeria = {
+      neighborhood: neighborhood[i], //check that it's singular
+      //each neighborhood will have an array of 18 sets (pizza, delivery, driver)
+      hourValues: hourStats
+    };
+    //push the neighborhood object to an array
+    finalArray.push(pizzeria);
+    //inner loop generates the countPizza, countDelivery, and countDriver by iterating through 18 hours of operation.
+    for(i = 0; i < hours.length; i++){
+        //create array to store the hours within the neighborhood object
+      var hourStats = [];
+        //push to the array that stores the object for each hours value
+        //Look through six scenarios
 
-//2pm to 4pm
-
-//5pm to 7pm
-
-//8pm to 10pm
-
-//11pm to 2am
+        //8am to 10am
+      if(i < 3){
+        minPizza = demand[0][1];
+        maxPizza = demand[0][2];
+        minDelivery = demand[0][3];
+        //11am to 1pm
+      } else if (i < 6){
+          minPizza = demand[1][1];
+          maxPizza = demand[1][2];
+          minDelivery = demand[1][3];
+        //2pm to 4pm
+        } else if (i < 9){
+          minPizza = demand[2][1];
+          maxPizza = demand[2][2];
+          minDelivery = demand[2][3];
+          //5pm to 7pm
+        } else if (i < 12){
+          minPizza = demand[3][1];
+          maxPizza = demand[3][2];
+          minDelivery = demand[3][3];
+          //8pm to 10pm
+        } else if (i < 15){
+          minPizza = demand[4][1];
+          maxPizza = demand[4][2];
+          minDelivery = demand[4][3];
+          //11pm to 2am
+        } else {
+          minPizza = demand[5][1];
+          maxPizza = demand[5][2];
+          minDelivery = demand[5][3];
+        }
+    }
+  }
+  console.table(finalArray);
+}

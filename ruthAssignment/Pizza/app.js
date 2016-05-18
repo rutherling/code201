2ab1d46c55910d1ca0d4682d8bc71bdfcd52dd02
RUTHERLING <rutherling@gmail.com>
2016-05-17 22:33:50 -0700

    started writing to sales-data.html

--- ruthAssignment/Pizza/app.js
+++ ruthAssignment/Pizza/app.js
@@ -1,12 +1,33 @@
 //write information from calculate.js to populate the index.html with #"happy pizza odysseys"
-
-//testing, sales id doesn't work because sumOperations is not defined.
-var softball = document.getElementById('storeHours');
-softball.textContent = "Herro?";
-
 var sales = document.getElementById('sales');
 sales.textContent = b[0] + " happy pizza odysseys this week!";
 
-
 //write information from calculate.js to populate sales-data.html with hourly sales data,
 //weekly sales data, and total operational information from all locations in tables.
+var operations = getElementById('operations');
+
+// Create <table> and <tr>
+var newTable = document.createElement('table');
+var newTR = document.createElement('tr');
+
+//Make <tr> a child of <table>
+newTable.appendChild(newTR);
+
+var locationHour = storeValues();//location by hour from calculate.js
+
+function showTable() {
+  var tds = []; //empty array for table data. This function converts array items into table data.
+  // Connect <td>'s as children of <tr>
+  for (var ii = 0; ii < locationHour.length; ii++) {
+    var newTD = document.createElement('td'); // New <td>
+    newTD.textContent = locationHour[ii]; // Fill in <td>'s with data from storeValues
+    newTR.appendChild(newTD); //<td> goes into <tr>
+    tds.push(newTD); //not sure what this does
+  }
+  return tds;
+};
+
+var myTDs = showTable();
+
+// Append <table> to DOM tree
+operations.appendChild(newTable);

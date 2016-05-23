a2f649e99de539ef6a87c35ad135d75796f977d3
RUTHERLING <rutherling@gmail.com>
2016-05-23 16:04:53 -0700

    CSS needs colons, not equal signs. adapted demo code for left img

--- week3/app.js
+++ week3/app.js
@@ -2,32 +2,67 @@
 //The callback should change the image file that is rendered inside the <img> element.
 //When the user clicks an image, it changse to a different image.
 
+//shortcut for "getElementById"
+function gebi(id) {
+  return document.getElementById(id);
+}
 //image directory
-var pictures =
-  [img/beardBeanie.jpg,
-  img/cornerFrame.jpg,
-  img/ctrlAltDel.jpg, 
-  img/beardBeanie,
-  img/beardBeanie,
-  img/beardBeanie,
-  img/beardBeanie,
-  img/beardBeanie,
-  img/beardBeanie,
-  img/beardBeanie];
+var imgNames = ['beardBeanie',
+                'boyfriendPillow',
+                'cornerFrame',
+                'ctrlAltDel',
+                'domeUmbrella',
+                'girlfriendPillow',
+                'pingpongDoor',
+                'pizzaScissors',
+                'slippersLED',
+                'travelPillow'];
 
-var leftImage = document.getElementbyId('leftImage');
-var rightImage = document.getElementbyId('rightImage');
+var images = [];
 
-function changeImage(){
-//similar to textContent, but with image content, replace the image with a different one from img directory.
-  leftImage.setAttribute('src','img/crazy-inventions-30.jpg');
-  //rightImage.src = 'img/funny-inventions.jpg';
-};
+leftImage = gebi('leftImage'); //creates a variable from DOM element, leftImage, so you can set the background-image url().
 
-// leftImage.onclick.changeImage;
-// rightImage.onclick.changeImage;
-//
-//Failed testing:
-leftImage.addEventListener('click', changeImage);
-rightImage.addEventListener('click', changeImage);
-//   // querySelector('leftImage');
+function getRandomInt() {
+  return Math.floor(imgNames.length * Math.random()); // idx is a random integer
+}
+
+function showNewImage(idx) { //accepts an index to retrieve an image from imgNames[].
+  leftImage.style['background-image'] = 'url("' + images[idx].src + '")';
+  leftImage.imageIdx = idx; // Store index of visible image
+  images[idx].incrementNshown();
+  var s = 'Show counts: ';
+  images.map(function(ele) { s += ele.Nshown + ', '; });
+  console.log(s);
+} //end .map to show the count of times the image has shown.
+
+function Image(src) { //creates an array of image objects. pass in imgNames[i] as src.
+  this.src = 'img/' + src + '.jpg';
+  this.Nclicks = 0;
+  this.Nshown = 0;
+  this.incrementClicks = function() {
+    this.Nclicks++;
+  }; //end incrementClicks method
+  this.incrementNshown = function() {
+    this.Nshown++;
+  }; //end incrementNshown method.
+} //end image constructor
+
+for (var ii = 0; ii < imgNames.length; ii++) {
+  var img = new Image(imgNames[ii]);
+  images.push(img);
+}//end for loop that creates image objects in the img array.
+
+showNewImage(getRandomInt()); //run showNewImage. Index is set from getRandomInt.
+console.log('leftImage.imageIdx = ' + leftImage.imageIdx);
+
+leftImage.addEventListener('click', refreshImage); //event to listen for, function to run.
+
+function refreshImage() {
+  images[leftImage.imageIdx].incrementClicks();
+  var s = 'click counts: ';
+  images.map(function(ele) { s += ele.Nclicks + ', '; });
+  console.log(s);
+
+  showNewImage(getRandomInt());
+  // Can use dot notation ("width" doesn't contain a dash)
+}

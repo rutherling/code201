9a5c9902bff5d0250f3e8eaee8c5b7ca5b9b602a
RUTHERLING <rutherling@gmail.com>
2016-05-23 14:28:21 -0700

    changed images, started array.

--- week3/app.js
+++ week3/app.js
@@ -2,13 +2,26 @@
 //The callback should change the image file that is rendered inside the <img> element.
 //When the user clicks an image, it changse to a different image.
 
+//image directory
+var pictures =
+  [img/beardBeanie.jpg,
+  img/cornerFrame.jpg,
+  img/ctrlAltDel.jpg, 
+  img/beardBeanie,
+  img/beardBeanie,
+  img/beardBeanie,
+  img/beardBeanie,
+  img/beardBeanie,
+  img/beardBeanie,
+  img/beardBeanie];
+
 var leftImage = document.getElementbyId('leftImage');
 var rightImage = document.getElementbyId('rightImage');
 
 function changeImage(){
 //similar to textContent, but with image content, replace the image with a different one from img directory.
-  leftImage.src = 'img/crazy-inventions-30.jpg';
-  rightImage.src = 'img/funny-inventions.jpg';
+  leftImage.setAttribute('src','img/crazy-inventions-30.jpg');
+  //rightImage.src = 'img/funny-inventions.jpg';
 };
 
 // leftImage.onclick.changeImage;

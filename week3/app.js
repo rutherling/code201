f757c50aa3eac6ae7538270612ada2a6683306e0
RUTHERLING <rutherling@gmail.com>
2016-05-24 22:41:56 -0700

    Bar chart displays. Next: percentages.

--- week3/app.js
+++ week3/app.js
@@ -120,17 +120,20 @@ function refreshImage(e) {
     //make the buttons visible
     var outcome = gebi('outcome');
     outcome.style['visibility'] = 'visible';
+    outcome.addEventListener('click', showCanvas);//end event listener. I wrote "drawCanvas" function.
+
+    var plot = gebi('visual');
+    plot.style['visibility'] = 'visible'; //show the div containing canvas drawing
+
     var voteAgain = gebi('voteAgain');
     voteAgain.style['visibility'] = 'visible';
 
     var container = gebi('imagesContainer'); //I need a way to leave the function so refreshImage stops running?
     container.style['display'] = 'none'; //doesn't work yet.
   }//end if for Nclicks comparison to 16.
+
+  NclicksArray(); //run function inside refreshImage(), but keep the return value in a global variable.
 } //end refreshImage
-
-//Event Listener for when user clicks "Show Results" button
-
-outcome.addEventListener('click', draw);//end event listener. I wrote "drawCanvas" function.
 
 //Array that matches src display names
 function nameArray(){
@@ -148,69 +151,35 @@ function NclicksArray(){
   for (var i = 0; i < images.length; i++) {
     countClick.push(images[i].Nclicks);
   }
-  return countClick;
-}
-var countClick = NclicksArray(); //assign the return value to a variable.
-//
+  console.log('countClick: ' + countClick);
+  return countClick; //returns blank, so you need to run the function each time you increment Nclicks.
+}//end NclicksArray
+//Event Listener for when user clicks "Show Results" button
+function showCanvas(){
+  var clickCount = NclicksArray(); //run global function NclicksArray.
 
-//Function to create canvas
-var productNames = [
-  'Coke',
-  'Pepsi',
-  'Sprite',
-  'Diet Coke',
-//'Mercedez',
-  'Orange Crush'];
-
-// var body = document.body;
-// console.log(body)
-// Didn't work:
-//   body.addEventListener('load', getRandomData);
-// This did work:
-//   body.addEventListener('click', getRandomData);
-
-// Same as window.addEventListener(...)
-addEventListener('load', drawRandomData);
-
-function draw(numArray, pcntArray, labelArray) {
-  var canvas = document.getElementById('canvas');
-
-  // **Shamelessly** copied from Chart.js documentation
-  var myChart = new Chart(canvas, {
-    type: 'bar',
-    data: {
-      labels: labelArray,
-      datasets: [{
-        label: '# of Votes',
-        data: numArray,
+  function drawCanvas (displayNames, clickCount){ //displayNames is global array.
+    var canvas = document.getElementById('canvas');
+    var myChart = new Chart(canvas, {
+      type: 'bar',
+      data: {
+        labels: displayNames,
+        datasets: [{
+          label: '# of Votes',
+          data: clickCount
+        }]
       },
-        {
-          type: 'line',
-          label: 'votes/shown %',
-          data: pcntArray
-        }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: { beginAtZero:true }
-        }]
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
       }
-    }
-  });
-}
-
-//draw(getRandomData());
-
-function drawRandomData() {
-  console.log('BLAAAAAH!');
-  var data = [];
-  var percents = [];
-  for (var ii = 0; ii < productNames.length; ii++) {
-    data.push(Math.ceil(Math.random() * 100));
-    percents.push(Math.ceil(Math.random() * 100));
-  }
-  console.log(data);
-  draw(data, percents, productNames);
-}
-//Function to draw canvas and show the parent container.
+    });
+  } //end drawCanvas
+  drawCanvas(displayNames, clickCount);
+  console.log('ShowCanvas ran.');
+}//end showCanvas
